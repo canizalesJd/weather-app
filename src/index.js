8207aae3d6301de56af38b94e84abed13f447d3d
Jose Canizales <canizalesjd@gmail.com>
2024-02-02 16:57:09 -0600

    General weather brief completed

--- src/index.js
+++ src/index.js
@@ -38,6 +38,7 @@ const selectLocation = (locationName, locationUrl) => {
 	searchLocationInput.value = locationName;
 	locationsContainer.innerHTML = "";
 	getCurrentWeather(locationUrl);
+	getForecastWeather(locationUrl);
 };
 
 const getCurrentWeather = async (locationUrl) => {
@@ -45,8 +46,8 @@ const getCurrentWeather = async (locationUrl) => {
 		`http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${locationUrl}`
 	);
 	const weather = await response.json();
-	console.log(weather);
-	updateWeather(weather);
+	updateCurrentWeather(weather);
+	updateCurrentWeatherIcon(weather);
 	return weather;
 };
 
@@ -62,11 +63,56 @@ const temperature = document.getElementById("temperature");
 const weatherStatusIcon = document.getElementById("weather-status-icon");
 const weatherDescription = document.getElementById("weather-description");
 const feelsLike = document.getElementById("feels-like");
+const pressure = document.getElementById("pressure");
 
-const updateWeather = async (weather) => {
-	const { temp_c, condition } = weather.current;
+const updateCurrentWeatherIcon = (weather) => {
+	const { condition } = weather.current;
+	const iconFolder = condition.icon.match(/day|night/g)[0];
+	const iconNumber = condition.icon.match(/\d+/g)[2];
+	const iconUrl = `assets/icons/${iconFolder}/${iconNumber}.svg`;
+	weatherStatusIcon.src = iconUrl;
+};
+
+const updateCurrentWeather = async (weather) => {
+	const { temp_c, condition, pressure_mb } = weather.current;
 	temperature.innerHTML = `${temp_c}°`;
 	weatherDescription.innerHTML = condition.text;
 	feelsLike.innerHTML = `Feels like ${weather.current.feelslike_c}°`;
-	return weather;
+	pressure.innerHTML = `${pressure_mb} mb`;
+};
+
+const getForecastWeather = async (
+	locationUrl,
+	days = 6,
+	airQuality = "no",
+	alerts = "yes"
+) => {
+	const response = await fetch(
+		`http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${locationUrl}&days=${days}&aqi=${airQuality}&alerts=${alerts}`
+	);
+	const forecast = await response.json();
+	updateForecast(forecast);
+};
+
+const minTemp = document.getElementById("min-temp");
+const maxTemp = document.getElementById("max-temp");
+const chanceOfRain = document.getElementById("chance-of-rain");
+const wind = document.getElementById("wind");
+const sunrise = document.getElementById("sunrise");
+const sunset = document.getElementById("sunset");
+const uvIndex = document.getElementById("uv-index");
+const humidity = document.getElementById("humidity");
+const gusts = document.getElementById("gusts");
+
+const updateForecast = (forecast) => {
+	const currentDayForecast = forecast.forecast.forecastday[0];
+	minTemp.innerHTML = `${currentDayForecast.day.mintemp_c}°`;
+	maxTemp.innerHTML = `${currentDayForecast.day.maxtemp_c}°`;
+	chanceOfRain.innerHTML = `${currentDayForecast.day.daily_chance_of_rain}%`;
+	wind.innerHTML = `${currentDayForecast.day.maxwind_kph} km/h`;
+	sunrise.innerHTML = `${currentDayForecast.astro.sunrise}`;
+	sunset.innerHTML = `${currentDayForecast.astro.sunset}`;
+	uvIndex.innerHTML = `${currentDayForecast.day.uv}`;
+	humidity.innerHTML = `${currentDayForecast.day.avghumidity}%`;
+	gusts.innerHTML = `${currentDayForecast.day.maxwind_kph} km/h`;
 };
