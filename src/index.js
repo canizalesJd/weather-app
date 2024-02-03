0a780af1493a82d8459f3ba40a35d416701b97fa
Jose Canizales <canizalesjd@gmail.com>
2024-02-02 20:00:03 -0600

    Updating https protocol

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const apiKey = "f9f500425848431297e232002243001";
 
 const searchLocation = async (searchTerm) => {
 	const response = await fetch(
-		`http://api.weatherapi.com/v1/search.json?key=${apiKey}&q=${searchTerm}`
+		`https://api.weatherapi.com/v1/search.json?key=${apiKey}&q=${searchTerm}`
 	);
 	const locations = await response.json();
 	displayLocations(locations);
@@ -44,7 +44,7 @@ const selectLocation = (locationName, locationUrl) => {
 
 const getCurrentWeather = async (locationUrl) => {
 	const response = await fetch(
-		`http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${locationUrl}`
+		`https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${locationUrl}`
 	);
 	const weather = await response.json();
 	updateCurrentWeather(weather);
@@ -87,7 +87,7 @@ const getForecastWeather = async (
 	alerts = "yes"
 ) => {
 	const response = await fetch(
-		`http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${locationUrl}&days=${days}&aqi=${airQuality}&alerts=${alerts}`
+		`https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${locationUrl}&days=${days}&aqi=${airQuality}&alerts=${alerts}`
 	);
 	const forecast = await response.json();
 	updateGeneralForecast(forecast);
